ba0565b22ac715899234fabb4847dc9bf894bf15
Darya Kuliashova <darya.kuliashova@generationimpact.global>
2025-08-08 09:32:21 +0200

    Add db

---
+++ data/tickets.sql
@@ -0,0 +1,17 @@
+INSERT INTO tickets (
+    customer_id, title, description, completed, tech, created_at, updated_at
+) VALUES (11, 'Laptop not powering on', 'Customer reported that their laptop does not power on even when plugged in.', false, 'unassigned', now(), now()),
+    (11, 'Slow computer performance', 'Customer mentioned that their computer is running very slowly.', false, 'unassigned', now(), now()),
+    (11, 'Virus removal', 'Customer needs virus removal service for their desktop computer.', false, 'unassigned', now(), now()),
+    (12, 'Screen replacement', 'Customer needs to replace a cracked screen on their laptop.', false, 'unassigned', now(), now()),
+    (12, 'Data recovery', 'Customer accidentally deleted important files and needs data recovery.', false, 'unassigned', now(), now()),
+    (12, 'Software installation', 'Customer requested installation of new software.', false, 'unassigned', now(), now()),
+    (13, 'Wi-Fi connectivity issues', 'Customer is experiencing problems connecting to Wi-Fi.', false, 'unassigned', now(), now()),
+    (13, 'Keyboard not working', 'Customer reported that the keyboard on their laptop is not functioning.', false, 'unassigned', now(), now()),
+    (13, 'Overheating problem', 'Customer''s computer is overheating and shutting down frequently.', false, 'unassigned', now(), now()),
+    (14, 'Hard drive replacement', 'Customer needs to replace a failing hard drive.', false, 'unassigned', now(), now()),
+    (14, 'Operating system upgrade', 'Customer requested an upgrade to the latest operating system.', false, 'unassigned', now(), now()),
+    (14, 'Battery replacement', 'Customer needs a replacement for their laptop battery.', false, 'unassigned', now(), now()),
+    (15, 'Printer setup', 'Customer needs help setting up a new printer.', false, 'unassigned', now(), now()),
+    (15, 'Blue screen error', 'Customer''s computer is showing a blue screen error frequently.', false, 'unassigned', now(), now()),
+    (15, 'Memory upgrade', 'Customer requested an upgrade to their computer’s RAM.', false, 'unassigned', now(), now());
